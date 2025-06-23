5e42c750ddf877a556908dff0e915b51ab68c16d
clem-field <kc8yhe@me.com>
2025-06-23 06:46:14 -0500

    added updated home page, corrected broken route

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,11 @@ Rails.application.routes.draw do
   get :callback, on: :member
   end
   # Adding omniauth
-  get "auth/:provider/callback", to: "sessions#create"
-  get "/login", to: "sessions#new"
+  # get "auth/:provider/callback", to: "sessions#create"
+  # get "/login", to: "sessions#new"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "catalog#index"
+  root "application#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
