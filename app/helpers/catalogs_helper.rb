f9680f10f118b9317d77e10b0ea00b7a78220662
clem-field <kc8yhe@me.com>
2025-06-29 15:54:24 -0500

    tables working, login working

---
+++ app/helpers/catalogs_helper.rb
@@ -0,0 +1,18 @@
+module CatalogsHelper
+  def sort_link(column:, label:)
+    direction = column ==params[:column] ? next_direction : "asc"
+    link_to(label, catalogs_path(column: column, direction: direction), data: { turbo_action: "replace" }, class: "table-header")
+  end
+
+  def next_direction
+    params[:direction] == "asc" ? "desc" : "asc"
+  end
+
+  def sort_indicator
+    tag.span(class: "sort sort-#{params[:direction]}")
+  end
+
+  def show_sort_indicator_for(column)
+    sort_indicator if params[:column] == column
+  end
+end
